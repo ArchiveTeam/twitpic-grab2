3cb0c36963cb19e8bc31c50000e8fa914dfb60fe
David Yip <yipdw@member.fsf.org>
2014-11-03 03:08:14 -0600

    upload utils: when moving to staging directory, clip out working dir.

--- util/upload/chunker.py
+++ util/upload/chunker.py
@@ -87,5 +87,5 @@ for spec in sys.stdin:
 
 	if current_group_size >= threshold:
 		print('group size is %s (>= %s), creating new group' % (current_group_size, threshold), file=sys.stderr)
-		os.rename(current_group_dir, '%s/%s' % (staging_dir, current_group_dir))
+		os.rename(current_group_dir, '%s/%s' % (staging_dir, os.path.basename(current_group_dir)))
 		del current_group_dirs[group]
